3b971600d70434006a32d1ee9c47d111ac2d4d8d
Leland T. Klauzer <lklauzer@gmail.com>
2021-05-03 12:38:26 -0700

    changed texts to inputs in ReservationForm

--- front-end/src/utils/components/ReservationForm.js
+++ front-end/src/utils/components/ReservationForm.js
@@ -3,30 +3,35 @@ function ReservationForm() {
   const renderView = (
     <form className="form-group">
       <label htmlFor="first_name">First name</label>
-      <text
+      <input
         className="form-control"
         id="first_name"
         name="first_name"
         required
       />
       <label htmlFor="last_name">Last name</label>
-      <text className="form-control" id="last_name" name="last_name" required />
+      <input
+        className="form-control"
+        id="last_name"
+        name="last_name"
+        required
+      />
       <label htmlFor="mobile_number">Mobile number</label>
-      <text
+      <input
         className="form-control"
         id="mobile_number"
         name="mobile_number"
         required
       />
       <label htmlFor="reservation_date">Reservation date</label>
-      <text
+      <input
         className="form-control"
         id="reservation_date"
         name="reservation_date"
         required
       />
       <label htmlFor="reservation_time">Reservation time</label>
-      <text
+      <input
         className="form-control"
         id="reservation_time"
         name="reservation_time"
